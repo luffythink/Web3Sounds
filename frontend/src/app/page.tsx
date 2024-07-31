71b515a431640965ac7458e0754213e9949d19ac
chenfang <cora1995@163.com>
2024-07-31 23:38:45 +0800

    init frontend
    Signed-off-by: chenfang <cora1995@163.com>

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h1 className="text-4xl font-bold mr-6 tracking-tight text-zinc-800 sm:text-5xl">
+      Web3 Sounds
+    </h1>
+  </main>;
+}

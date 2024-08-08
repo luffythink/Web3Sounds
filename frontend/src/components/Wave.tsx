ef60967f6d47e71a1f887accdeb7906bfd15edcc
chenfang <cora1995@163.com>
2024-08-08 13:06:38 +0800

    fix: type error
    Signed-off-by: chenfang <cora1995@163.com>

--- frontend/src/components/Wave.tsx
+++ frontend/src/components/Wave.tsx
@@ -1,12 +1,12 @@
 export function Wave() {
   return (
     <div className="active ml-2 flex items-center justify-center space-x-1 align-middle">
-      <div className="animate-wave w-1 bg-green-600" />
-      <div className="animate-wave2 w-1 bg-green-600" />
-      <div className="animate-wave3 w-1 bg-green-600" />
-      <div className="animate-wave4 w-1 bg-green-600" />
-      <div className="animate-wave5 w-1 bg-green-600" />
-      <div className="animate-wave6 w-1 bg-green-600" />
+      <div className="w-1 animate-wave bg-green-600" />
+      <div className="w-1 animate-wave2 bg-green-600" />
+      <div className="w-1 animate-wave3 bg-green-600" />
+      <div className="w-1 animate-wave4 bg-green-600" />
+      <div className="w-1 animate-wave5 bg-green-600" />
+      <div className="w-1 animate-wave6 bg-green-600" />
     </div>
   );
 }
